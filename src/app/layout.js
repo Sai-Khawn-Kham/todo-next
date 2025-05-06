66dcfb5717fb7c4b93f821c34816ca2b5b61b67c
Sai Khawn Kham <khawnkham222@gmail.com>
2025-05-06 07:13:12 +0630

    fix: web title

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,11 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Task Manager</title>
+      </head>
       <body className="p-3">
         {children}
       </body>
